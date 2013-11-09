dd4dc043e14dd22eece9c08eb92a1e21b5b52603
Juho Vepsalainen <bebraw@gmail.com>
2013-11-09 12:28:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# itspreadsheet2rest
---
+++ test.js
@@ -0,0 +1,16 @@
+var GoogleSpreadsheet = require("google-spreadsheet");
+
+var parse = require('./lib/parse');
+
+
+main();
+
+function main() {
+    var sheet = new GoogleSpreadsheet('0AhdUvwJs9WNSdFRiYkdGcU9FT3RIdEhaM2F0VU8tRUE');
+
+    parse(sheet, function(err, d) {
+        if(err) return console.error(err);
+
+        console.log(d.slice(10));
+    });
+}
