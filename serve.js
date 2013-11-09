cd60dcc9708830f5c13b29ec900b49c91a565788
Juho Vepsalainen <bebraw@gmail.com>
2013-11-09 13:08:29 +0200

    Set up basic server

---
+++ serve.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+var express = require('express');
+
+var config = require('./config');
+
+
+main();
+
+function main() {
+    var app = express();
+
+    var apiPrefix = 'v1';
+
+    var port = config.port;
+
+    app.configure(function() {
+        app.set('port', port);
+
+        app.use(express.logger('dev'));
+
+        app.use(app.router);
+    });
+
+    app.configure('development', function() {
+        app.use(express.errorHandler());
+    });
+
+    app.get('/' + apiPrefix + '/companies', function(req, res) {
+        // TODO: return data.json
+
+        res.send(200);
+    });
+    // TODO: queries (search using query parameter
+
+    // TODO: cron
+
+    process.on('exit', terminator);
+
+    ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS',
+    'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGPIPE', 'SIGTERM'
+    ].forEach(function(element, index, array) {
+        process.on(element, function() { terminator(element); });
+    });
+
+    app.listen(port, function() {
+        console.log('%s: Node (version: %s) %s started on %d ...', Date(Date.now() ), process.version, process.argv[1], port);
+    });
+}
+
+function terminator(sig) {
+    if(typeof sig === "string") {
+        console.log('%s: Received %s - terminating Node server ...',
+            Date(Date.now()), sig);
+
+        process.exit(1);
+    }
+
+    console.log('%s: Node server stopped.', Date(Date.now()) );
+}
